c3b5f2ce1299f3816324be89f3cd865eb64eae8f
Juan Ignacio Coronel <phjocoronel@gmail.com>
2021-09-13 20:26:41 -0300

    example credentials notificator

--- backend/src/settings/controllers.ts
+++ backend/src/settings/controllers.ts
@@ -58,8 +58,21 @@ const controllerSettings: ControllerSettings = {
         // }
     },
     notificator: {
-        senders: {},
-        reciver: {}
+        senders: {
+            // mail: {
+            //     user: 'phjocoronel17@gmail.com',
+            //     pass: 'FeZb13392128'
+            // },
+            // twilio: {
+            //     account_sid: 'AC7230ab8c825e1d345e1402967c87dd7d',
+            //     auth_token: '8125f99d1fe862acf6a2a594d27db921',
+            //     phone: '+16174544586'
+            // }
+        },
+        reciver: {
+            // mail: 'phjocoronel@gmail.com',
+            // phone: '+542396610172'
+        }
     }
 }
 
